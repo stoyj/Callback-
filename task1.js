b44e84391b3472396d1e8204798dfbf0898f6eb9
stoyj <stoyanka.kamenova@gmail.com>
2025-01-30 13:06:15 +0200

    Задача 1: Основен Callback Задача 2: Callback с параметър

--- task1.js
+++ task1.js
@@ -1,3 +1,6 @@
+// Задача 1: Основен Callback
+
+// Задача: Напиши функция processMessage, която приема съобщение и callback функция. Callback функцията трябва да бъде извикана след като съобщението бъде отпечатано.
 function processMessage(message, callback) {
   console.log("Събщение:" + message);
   // callback();
@@ -8,4 +11,19 @@ function done() {
 }
 
 // Test
-processMessage("Здравей, свят!");
+processMessage("Здравей, свят!"); // Съобщение: Здравей, свят! // Обработката е завършена!
+
+// Задача 2: Callback с параметър
+
+// Задача: Напиши функция calculate, която приема две числа и callback функция, която ще извърши математическа операция върху тях.
+
+function calculate(a, b, callback) {
+  return callback(a, b);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+// Test
+console.log(calculate(5, 3, add)); // Резултат 8
